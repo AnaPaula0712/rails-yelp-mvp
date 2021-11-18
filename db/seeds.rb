c5e8d385308c6037174b2fa90d97ae755099471c
Ana Westphalen <campos1984@hotmail.com>
2021-11-18 08:49:07 -0300

    MVC advanced done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: %w[chinese italian japanese french belgian].sample,
+    phone_number: Faker::PhoneNumber.phone_number
+  )
+  rand(5..10).times do
+    Review.create!(
+      restaurant: restaurant,
+      content: Faker::Restaurant.review,
+      rating: rand(0..5)
+    )
+  end
+end
